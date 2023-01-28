1065b643e22d8b4b097b779257d1c895372a39fa
Christian Tsegaye <C.Tsegaye@outlook.com>
2023-01-28 00:57:29 -0500

    Initial commit

---
+++ src/Fighters.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Fighters{
+    
+    HashMap<String, Integer> Selection = new HashMap<String, Integer>();
+
+    
+    public Fighters(){
+        Selection.put("Faust", 1);
+        Selection.put("Happy Chaos", 2);
+        Selection.put("May", 1);
+        Selection.put("Baiken", 0);
+    }
+}
